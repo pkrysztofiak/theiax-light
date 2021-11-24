c591d4f54dcce41eb74b1177d206c98146646959
p.krysztofiak <p.krysztofiak@pixel.com.pl>
2021-11-24 09:27:35 +0100

    Add JUnit5 dependency. Create first test Closes #17

---
+++ theiax-presentation-model/src/test/java/com/theiax/presentationmodel/domain/GridCellTest.java
@@ -0,0 +1,14 @@
+package com.theiax.presentationmodel.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GridCellTest {
+
+    @Test
+    void updateBounds() {
+        Assertions.assertEquals("true", "true");
+    }
+}
